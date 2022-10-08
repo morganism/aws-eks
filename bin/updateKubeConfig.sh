ec4a53cc424d3771b7a8f1f04f9c5dfe716f6433
Morgan Sziraki <morgan.sziraki@gmail.com>
2022-10-08 05:17:00 +0100

    DEV-001 new branch for actions and commands (#1)

---
+++ bin/updateKubeConfig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws eks --region eu-west-2 update-kubeconfig --name demo-eks-ZpHCKd4o
--- helm_bashcompletion.sh
+++ helm_bashcompletion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 helm completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
+#. /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 source <(helm completion bash)
 
