89648446debd5019683be89f476661858a6ccf3a
Morgan Sziraki <morgan.sziraki@gmail.com>
2022-10-12 12:39:49 +0100

    DEV-001 add kong deployments

--- deploy_kong_ingress_controller.sh
+++ deploy_kong_ingress_controller.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-CLUSTER_NAME=`aws eks list-clusters | bin/getClusterName.rb`
+CLUSTER_NAME=`aws eks list-clusters | ruby -rjson -e "puts JSON.parse(STDIN.read)['clusters']"`
+echo "Cluster name is ${CLUSTER_NAME}"
 
+echo "Creating Kong Ingress Controller"
 kubectl create -f https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/v2.7.0/deploy/single/all-in-one-dbless.yaml
+
+echo "Updating kubeconfig"
 aws sts get-caller-identity
 aws eks --region eu-west-2 update-kubeconfig --name ${CLUSTER_NAME}
+
+echo "Getting service info"
 kubectl get svc
+
+echo "Adding Helm charts and namespace"
 helm repo add kong https://charts.konghq.com
 helm repo update
 helm install kong/kong --generate-name --set ingressController.installCRDs=false
